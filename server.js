80c595a07bf2665ea7b23b143d0a5a50b446e65e
unknown <chemicalufo@gmail.com>
2017-11-11 15:00:10 +0200

    asdasd

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
+app.get('/petro', function(req, res) {
+    res.send('Hello Petro!');
+});
+
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
