48985470965b9d39d2a77d6d490389cb2695985c
juljk <julikjulik22@gmail.com>
2017-11-13 11:18:20 +0200

    Calc form

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static("files"));
+
 app.get('/', function(req, res) {
     res.send('Hello World!');
-});
-
-app.get('/petro', function(req, res) {
-    res.send('Hello Petro!');
 });
 
 
